267b17c3cd1df82555cd3a9b2e3d36a4d63d7c14
lordcsd <dimgbachinonso@gmail.com>
2023-04-17 12:16:04 +0100

    more on error handing

--- src/decorators/enum.decorator.ts
+++ src/decorators/enum.decorator.ts
@@ -1,10 +1,10 @@
-import { IsOptional } from 'class-validator';
-import { ICustomEnumValidatorOptions } from '../dto/customValidatorOptions.dto';
+import { IsOptional } from "class-validator";
+import { ICustomEnumValidatorOptions } from "../dto/customValidatorOptions.dto";
 import {
   swaggerProp,
   notEmptyFn,
   isEnumFn,
-} from '../utils/commonDecoratorFunctions';
+} from "../utils/commonDecoratorFunctions";
 
 export function CustomEnumValidator(details: ICustomEnumValidatorOptions) {
   const { validEnum, optional, defaultValue, description, isArray } = details;
@@ -13,13 +13,21 @@ export function CustomEnumValidator(details: ICustomEnumValidatorOptions) {
     validEnum,
     optional,
     description,
-    defaultValue: `${
-      optional ? 'optional enum, example:' : ''
-    }${defaultValue}`,
+    defaultValue: defaultValue || defaultValue[Object.values(validEnum)[0]],
     isArray,
   });
 
   return function (target: any, key: string) {
+    if (!validEnum) {
+      throw new Error(`Please provide a validEnum for ${target}.${key}`);
+    }
+
+    if (!validEnum[defaultValue]) {
+      throw new Error(
+        `Please provide valid default value for  ${target}.${key}`
+      );
+    }
+
     optional ? IsOptional()(target, key) : notEmptyFn(key)(target, key);
     notEmptyFn(key)(target, key);
     isEnumFn(key, validEnum as object, isArray)(target, key);
